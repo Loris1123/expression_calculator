b3766c1860ce47df5c946d880b949759287a8349
Loris1123 <molottermann@gmail.com>
2013-03-01 19:00:40 +0100

    Headcomments and improved output

--- ExpressionCalculator/main.go
+++ ExpressionCalculator/main.go
@@ -1,3 +1,11 @@
+/*
+* Author: https://github.com/Loris1123
+* Date: 2013-03-1
+* Last Update: 2013-03-1
+* File: main.go
+* Main file for the expression calculator.
+*/
+
 package main
 
 import (
@@ -5,7 +13,7 @@ import (
 	"flag"
 )
 
-var a,b,c,d float32
+var a,b,c,d float64
 var expression string
 
 func main(){
@@ -20,10 +28,10 @@ func main(){
 }
 
 func initFlags(){
-	flag.IntVar(&a,"a",0, "Value for a")
-	flag.IntVar(&b,"b",0, "Value for a")
-	flag.IntVar(&c,"c",0, "Value for a")
-	flag.IntVar(&d,"d",0, "Value for a")
+	flag.Float64Var(&a,"a",0, "Float value for a")
+	flag.Float64Var(&b,"b",0, "Float value for b")
+	flag.Float64Var(&c,"c",0, "Float value for c")
+	flag.Float64Var(&d,"d",0, "Float value for d")
 	flag.StringVar(&expression,"exp","","The expression to be calculated")
 	flag.Parse()
 }
